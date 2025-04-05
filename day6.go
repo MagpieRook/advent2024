a180032a21ce8b297ef3ce7ac0a2d79c059640d7
Ali Rook <ali@rook.me>
2025-04-05 13:46:50 -0400

    Day 6 done finally

--- day6.go
+++ day6.go
@@ -23,13 +23,8 @@ func changeFacing(facing [2]int) [2]int {
 	panic("Invalid input")
 }
 
-// how does this brute force solution not work D:
-// am I overcounting??
-// TODO: the correct answer is 1836, this returns 1913????
 func checkForLoop(currentPos, facing [2]int, obstacles map[int][]int, width, height int) bool {
 	checkPos := [2]int{currentPos[0] + facing[0], currentPos[1] + facing[1]}
-	newObstacles := maps.Clone(obstacles)
-	newObstacles[checkPos[0]] = append(newObstacles[checkPos[0]], checkPos[1])
 	visited := map[[2]int][][2]int{}
 	for currentPos[0] < height && currentPos[0] >= 0 && currentPos[1] < width && currentPos[1] >= 0 {
 		if slices.Contains(visited[currentPos], facing) {
@@ -40,7 +35,7 @@ func checkForLoop(currentPos, facing [2]int, obstacles map[int][]int, width, hei
 
 		checkPos[0] = currentPos[0] + facing[0]
 		checkPos[1] = currentPos[1] + facing[1]
-		if slices.Contains(newObstacles[checkPos[0]], checkPos[1]) {
+		if slices.Contains(obstacles[checkPos[0]], checkPos[1]) {
 			facing = changeFacing(facing)
 			continue
 		}
@@ -63,23 +58,23 @@ func main() {
 	// guard steps forward each turn
 	// when guard reaches an obstruction, turn right 90 degrees
 	obstacleMap := map[int][]int{}
+	newObstaclePositions := map[int][]int{}
 	var startingPos [2]int
 	rows := strings.Split(string(input), "\n")
 	for y, row := range rows {
 		for x, col := range row {
 			if col == '^' {
 				startingPos = [2]int{y, x}
-			}
-			if col == '#' {
+			} else if col == '#' {
 				obstacleMap[y] = append(obstacleMap[y], x)
+			} else {
+				newObstaclePositions[y] = append(newObstaclePositions[y], x)
 			}
 		}
 	}
-	currentPos := [2]int{}
-	currentPos[0], currentPos[1] = startingPos[0], startingPos[1]
+	currentPos := [2]int{startingPos[0], startingPos[1]}
 	facing := [2]int{-1, 0}
 	visited := [][2]int{startingPos}
-	possibleObstructions := [][2]int{}
 	width, height := len(rows[currentPos[1]]), len(rows)
 	checkPos := [2]int{}
 	for currentPos[0] < height && currentPos[0] >= 0 && currentPos[1] < width && currentPos[1] >= 0 {
@@ -95,16 +90,27 @@ func main() {
 			continue
 		}
 
-		// part 2: check for ways to make loops with new obstructions
-		// we know the next position isn't an obstruction, so let's try adding one
-		if !slices.Contains(possibleObstructions, checkPos) &&
-			checkForLoop(currentPos, facing, obstacleMap, width, height) {
-			possibleObstructions = append(possibleObstructions, checkPos)
-		}
-
 		currentPos[0], currentPos[1] = checkPos[0], checkPos[1]
 	}
 
 	fmt.Println(len(visited))
+
+	// part 2: brute force edition II
+	// just check every position, who cares if it takes 4 years if I get the right answer
+	possibleObstructions := [][2]int{}
+	facing = [2]int{-1, 0}
+	for y, row := range newObstaclePositions {
+		for _, col := range row {
+			newObstacleMap := maps.Clone(obstacleMap)
+			newObstacleMap[y] = append(newObstacleMap[y], col)
+			if checkForLoop(startingPos, facing, newObstacleMap, width, height) {
+				possibleObstructions = append(possibleObstructions, [2]int{y, col})
+			}
+		}
+	}
+
+	// for some reason, this solution gets 1836 (the correct answer) whereas the earlier version got 1913
+	// something about following the path the person was taking rather than checking all positions, I guess
+	// whatever we got there in the end
 	fmt.Println(len(possibleObstructions))
 }
