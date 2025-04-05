f903bff469a5d20ef9e8c2d5de559e0e9f8d05e3
Ali Rook <ali@rook.me>
2025-04-05 02:42:03 -0400

    It works on the example, it might be time to give up on day 6

--- day6.go
+++ day6.go
@@ -20,25 +20,26 @@ func changeFacing(facing [2]int) [2]int {
 	} else if facing[1] == 1 {
 		return [2]int{1, 0}
 	}
-	return [2]int{0, 0}
+	panic("Invalid input")
 }
 
 // how does this brute force solution not work D:
+// am I overcounting??
+// would it be faster to do this by hand at this point? maybe
 func checkForLoop(currentPos, facing [2]int, obstacles map[int][]int, width, height int) bool {
 	checkPos := [2]int{currentPos[0] + facing[0], currentPos[1] + facing[1]}
 	newObstacles := maps.Clone(obstacles)
 	newObstacles[checkPos[0]] = append(newObstacles[checkPos[0]], checkPos[1])
 	visited := map[[2]int][][2]int{}
-	for checkPos[0] < width && checkPos[0] >= 0 && checkPos[1] < height && checkPos[1] >= 0 {
-		checkPos[0] = currentPos[0] + facing[0]
-		checkPos[1] = currentPos[1] + facing[1]
-
+	for currentPos[0] < height && currentPos[0] >= 0 && currentPos[1] < width && currentPos[1] >= 0 {
 		if slices.Contains(visited[currentPos], facing) {
 			return true
 		}
 
 		visited[currentPos] = append(visited[currentPos], facing)
 
+		checkPos[0] = currentPos[0] + facing[0]
+		checkPos[1] = currentPos[1] + facing[1]
 		if slices.Contains(newObstacles[checkPos[0]], checkPos[1]) {
 			facing = changeFacing(facing)
 			continue
@@ -50,26 +51,24 @@ func checkForLoop(currentPos, facing [2]int, obstacles map[int][]int, width, hei
 
 func main() {
 	// input is a grid with:
-	//   - an arrow (^/>/</v) for guards
+	//   - an arrow (^/>/</v) for guard starting position
 	//   - a # for obstructions the guard can't see or move through
 	//   - a . everywhere else
-	// part 1 goal: mark every tile the guard will stand on
-	// guard steps forward each turn
-	// when guard reaches an obstruction, turn right 90 degrees
-	input, err := os.ReadFile("day6input.txt")
+	input, err := os.ReadFile("day6test.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	startingPos := [2]int{}
-	// a map of obstacles by row and column
+	// part 1 goal: mark every tile the guard will stand on
+	// guard steps forward each turn
+	// when guard reaches an obstruction, turn right 90 degrees
 	obstacleMap := map[int][]int{}
+	var startingPos [2]int
 	rows := strings.Split(string(input), "\n")
 	for y, row := range rows {
 		for x, col := range row {
 			if col == '^' {
-				startingPos[0] = y
-				startingPos[1] = x
+				startingPos = [2]int{y, x}
 			}
 			if col == '#' {
 				obstacleMap[y] = append(obstacleMap[y], x)
@@ -81,8 +80,9 @@ func main() {
 	facing := [2]int{-1, 0}
 	visited := [][2]int{startingPos}
 	possibleObstructions := [][2]int{}
-	for {
-		checkPos := [2]int{}
+	width, height := len(rows[currentPos[1]]), len(rows)
+	checkPos := [2]int{}
+	for currentPos[0] < height && currentPos[0] >= 0 && currentPos[1] < width && currentPos[1] >= 0 {
 		checkPos[0] = currentPos[0] + facing[0]
 		checkPos[1] = currentPos[1] + facing[1]
 		onExistingPath := slices.Contains(visited, currentPos)
@@ -97,12 +97,9 @@ func main() {
 
 		// part 2: check for ways to make loops with new obstructions
 		// we know the next position isn't an obstruction, so let's try adding one
-		if checkForLoop(currentPos, facing, obstacleMap, len(rows[currentPos[1]]), len(rows)) {
+		if !slices.Contains(possibleObstructions, checkPos) &&
+			checkForLoop(currentPos, facing, obstacleMap, width, height) {
 			possibleObstructions = append(possibleObstructions, checkPos)
-		}
-
-		if checkPos[0] >= len(rows[currentPos[1]]) || checkPos[0] < 0 || checkPos[1] >= len(rows) || checkPos[1] < 0 {
-			break
 		}
 
 		currentPos[0], currentPos[1] = checkPos[0], checkPos[1]
